4514014e51544cfecd67c6d34052a54397462808
Terekhin <blackrat72@qip.ru>
2022-12-17 16:28:19 +0300

    add structure

---
+++ remove_pupil.py
@@ -0,0 +1,15 @@
+#
+
+def remove_pupil():
+    print("действие обратное add_pupil()")
+    print("при удалении ученика надо также удалить из списка rating.csv все предметы по его ID ")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    remove_pupil()
