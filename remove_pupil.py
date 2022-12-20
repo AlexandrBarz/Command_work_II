abb35fb60cbf63b1570aa73b17f15532f98fcdcf
kate <k-b-v@yandex.ru>
2022-12-20 15:43:06 +0300

    add block write

--- remove_pupil.py
+++ remove_pupil.py
@@ -2,45 +2,69 @@ from functions import read_from_csv
 
 
 from functions import write_list_to_csv
+import csv
 
-# name_str
+
 
 
 def remove_pupil():
-    id_pupil = '' 
+    id_pupil2 = id_pupil  
     
 
     path_file = 'pupil.csv'
     path_file_rating = 'rating.csv'
     
 
-    pupil_list = read_from_csv(path_file)
+    pupil_list = read_from_csv(path_file, coding='UTF-8', delim = '|')
    
-    rating_list = read_from_csv(path_file_rating) 
+    rating_list = read_from_csv(path_file_rating, coding='UTF-8', delim = '|') 
     
   
-    name_str = input('введите Фамилию и Имя ученика через пробел: > ')
-    name= name_str.split()
-    id_pupil2 = id_pupil 
-    for row in pupil_list: 
+    name_str = input('Введите Фамилию и Имя ученика через пробел: > ')
+    verify_delete  = input(f"Уважаемый, ты уверен, что хочешь удалить ученика из журнала?"
+                           f"\nНапишите 'да'/'нет'/'q'(выход в меню)\n> ")
+    if verify_delete == "да":
+        name= name_str.split()
+       
+        
+    
+        for row in pupil_list: 
                        
-        if row[1].lower() == name[0].lower() and row[2].lower() == name[1].lower():
-            id_pupil = row[0]
-            pupil_list.remove(row)
-    for row in rating_list:
-        if row[0].lower() == id_pupil2:
-            rating_list.remove(row)
+            if row[1].lower() == name[0].lower() and row[2].lower() == name[1].lower():
+               
+                id_pupil = row[0].lower()
+                
+                pupil_list.remove(row)
+        for row in rating_list:
+            if row[0].lower() == id_pupil2:
+                rating_list.remove(row)
+            
+            with open(path_file, 'w', encoding='UTF-8') as pupil_list:
+                file_writer = csv.writer(pupil_list, delimiter = "|" , lineterminator="\n")
+                for row in pupil_list:       
+                    file_writer.writerow(row) 
 
+            with open(path_file_rating, 'w', encoding='UTF-8') as rating_list:
+                file_writer = csv.writer(rating_list, delimiter = "|" , lineterminator="\n")
+                for row in rating_list:       
+                    file_writer.writerow(row) 
+    
+           
+        print('Ученик успешно удален')        
+    else:
+        pass
+    
+    # row_del = [row for row in rating_list if row[0]==id_pupil2] (можно в таком виде?)
+    # rating_list.remove(row_del)
+        
+    
+       
 
     # Совсем не уверена, что это может работать(((
     #  Может быть надо совсем по другой логике пойти. 
 
-
-   
-    
-    pupil_list = write_list_to_csv(path_file)
-    print("действие обратное add_pupil()")
-    print("при удалении ученика надо также удалить из списка rating.csv все предметы по его ID ")
+    # print("действие обратное add_pupil()")
+    # print("при удалении ученика надо также удалить из списка rating.csv все предметы по его ID ")
 
 
 
