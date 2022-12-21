b03327f856eea537f7ca601b0e97409cefbfff9b
kate <k-b-v@yandex.ru>
2022-12-21 12:58:17 +0300

    Working

--- remove_pupil.py
+++ remove_pupil.py
@@ -7,22 +7,49 @@ import csv
 
 
 
-def remove_pupil():
+def id_pupil():
     id_pupil  = ''
-    id_pupil2 =''
+    '''Находит id по имени и фамилии'''
+    
      
     
+
+    path_file = 'pupil.csv'
+   
+    
+
+    pupil_list = read_from_csv(path_file, coding='UTF-8', delim = '|')
+   
+    
+  
+    name_str = input('Введите Фамилию и Имя ученика через пробел: > ')
+   
+    name= name_str.split()
+       
+        
+    
+    for row in pupil_list: 
+                       
+        if row[1].lower() == name[0].lower() and row[2].lower() == name[1].lower():
+                
+            id_pupil = row[0].lower()
+            return (id_pupil)
+
+def remove_pupil():
+    ''' находим нужное имя (и дополнительно проверям того ли
+     ученика планирует удалить пользователь. Далее удаляем ученика из списка
+     учеников, и ученика по id из rating'''
+
+    id_pupil_del = id_pupil() #записываем нужный id в переменную
 
     path_file = 'pupil.csv'
     path_file_rating = 'rating.csv'
     
 
     pupil_list = read_from_csv(path_file, coding='UTF-8', delim = '|')
-   
     rating_list = read_from_csv(path_file_rating, coding='UTF-8', delim = '|') 
-    
-  
-    name_str = input('Введите Фамилию и Имя ученика через пробел: > ')
+
+    name_str = input('Еще раз введите Фамилию и Имя ученика через пробел: > ')
     verify_delete  = input(f"Уважаемый, ты уверен, что хочешь удалить ученика из журнала?"
                            f"\nНапишите 'да'/'нет'/'q'(выход в меню)\n> ")
     if verify_delete == "да":
@@ -30,50 +57,45 @@ def remove_pupil():
        
         
     
-        for row in pupil_list: 
+    for row in pupil_list: 
                        
-            if row[1].lower() == name[0].lower() and row[2].lower() == name[1].lower():
-                id_pupil2 = id_pupil 
-                id_pupil = row[0].lower()
-                
-                pupil_list.remove(row)
-        for row in rating_list:
-            if row[0].lower() == id_pupil2:
-                rating_list.remove(row)
-            
-            with open(path_file, 'w', encoding='UTF-8') as p_list:
-                file_writer = csv.writer(p_list, delimiter = "|" , lineterminator="\n")
-                for row in pupil_list:       
-                    file_writer.writerow(row) 
+        if row[1].lower() == name[0].lower() and row[2].lower() == name[1].lower():
+            pupil_list.remove(row)
+            ''' удаляем имя из списка учеников'''
 
-            with open(path_file_rating, 'w', encoding='UTF-8') as r_list:
-                file_writer = csv.writer(r_list, delimiter = "|" , lineterminator="\n")
-                for row in rating_list:       
-                    file_writer.writerow(row) 
+        for row in rating_list:
+            if row[0].lower() == id_pupil_del:
+                rating_list.remove(row)
+                ''' удаляем id из списка учеников'''
+
+         
+                
+    
+            
+        with open(path_file, 'w', encoding='UTF-8') as p_list:
+            file_writer = csv.writer(p_list, delimiter = "|" , lineterminator="\n")
+            for row in pupil_list:       
+                 file_writer.writerow(row) 
+
+        with open(path_file_rating, 'w', encoding='UTF-8') as r_list:
+            file_writer = csv.writer(r_list, delimiter = "|" , lineterminator="\n")
+            for row in rating_list:       
+                file_writer.writerow(row) 
     
            
         print('Ученик успешно удален')        
     else:
         pass
     
-    # row_del = [row for row in rating_list if row[0]==id_pupil2] (можно в таком виде?)
-    # rating_list.remove(row_del)
+  
         
     
        
 
-    # Совсем не уверена, что это может работать(((
-    #  Может быть надо совсем по другой логике пойти. 
-
-    # print("действие обратное add_pupil()")
-    # print("при удалении ученика надо также удалить из списка rating.csv все предметы по его ID ")
+   
 
 
 
 
-
-
-
-
-# if __name__ == '__main__':
-#     remove_pupil()
+if __name__ == '__main__':
+    remove_pupil()
